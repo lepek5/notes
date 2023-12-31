45653970224c680d98277c3b5a61e59f6b140365
lepek5 <jari.mikkonen@gmail.com>
2023-12-31 16:23:12 +0200

    github actions init

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,6 @@ module.exports = {
   modulePathIgnorePatterns: ['<rootDir>/build/'],
   testMatch: ['**/*.test.ts'],
   setupFiles: ['<rootDir>/src/tests/setupTests.ts'],
-  setupFilesAfterEnv: ['<rootDir>/src/tests/jest.setup.ts'],
   transform: {
     '^.+\\.{ts|tsx}?$': ['ts-jest', {
       babel: true,
